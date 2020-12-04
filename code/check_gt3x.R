bd7c917bcb17499438b122a1ae8fb74080943f50
muschellij2 <muschellij2@gmail.com>
2020-12-04 11:44:11 -0500

    updated

--- code/check_gt3x.R
+++ code/check_gt3x.R
@@ -111,7 +111,22 @@ for (iid in seq(nrow(df))) {
   st = sub_thing(hdr, "Start Time")
   sd = sub_thing(hdr, "Start Date")
   srate = as.numeric(sub(".*at (\\d*) Hz.*", "\\1", hdr[1]))
-  start_date = lubridate::mdy_hms(paste0(sd, " ", st))
+  
+  format = sub(".*date format (.*) at.*", "\\1", hdr[1])
+  if (format == "") {
+    warning("No format for date in the header, using mdy")
+    format = "mdy"
+  } else {
+    format = tolower(format)
+    format = c(sapply(strsplit(format, "/"), substr, 1,1))
+    format = paste(format, collapse = "")
+  }
+  all_formats = c("ydm", "dym", "ymd", "myd", "dmy", "mdy")
+  stopifnot(format %in% all_formats)
+  lubridate_func = paste0(format, "_hms")
+  lubridate_func = getFromNamespace(lubridate_func, "lubridate")
+  start_date = do.call(lubridate_func, args = list(paste0(sd, " ", st)))
+  
   
   csv$time = seq(0, nrow(csv) - 1)/srate
   csv$time = start_date + csv$time
