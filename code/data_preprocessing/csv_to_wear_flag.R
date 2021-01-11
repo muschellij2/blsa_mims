275bd259827e0979106a8cb018993c13b502889f
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-01-11 17:50:05 -0500

    update the csv_to_wear_flag code

--- code/data_preprocessing/csv_to_wear_flag.R
+++ code/data_preprocessing/csv_to_wear_flag.R
@@ -1,5 +1,3 @@
-#!/usr/bin/env Rscript
-
 #' This script iterates over Actilife-outputed CSV files with minute-level 
 #' activity counts and computes minute-level wear/non-wear flag (\code{1}/\code{0})
 #' for each minute of activity counts data.
@@ -10,16 +8,21 @@
 #' 
 #' input files: /csv/
 #' output files: /csv_wear_flag/
+#' 
+#' use: 
+#' cd /dcl01/smart/data/activity/blsa_mims
+
+rm(list = ls())
 
 library(dplyr)
 library(data.table)
 library(lubridate)
+library(arctools)
 
 fpaths = sort(list.files(path = paste0(here::here(), "/csv"), full.names = TRUE, pattern = "[.]csv"))
 fpaths_l <- length(fpaths)
-# fidx =  as.numeric(as.character(args[1])) # as.numeric(Sys.getenv("SGE_TASK_ID"))
 
-for (fidx in 1:10){ # fidx <- 1
+for (fidx in 1:fpaths_l){ # fidx <- 1
   message(paste0("fidx: ", fidx))
   # ActiGraph AC file path
   fpath = fpaths[fidx]
