356accbdb9c15af13095f9237a1a5cbc26bf4776
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-01-14 14:12:38 -0500

    add line to address issue with MIMSunit package that allows negative values in the output

--- code/data_preprocessing/mat_to_open_source_measures.R
+++ code/data_preprocessing/mat_to_open_source_measures.R
@@ -37,7 +37,7 @@ fname = fnames[ifile]
 id = sub("RAW[.]mat", "", basename(fname))
 message(fname)
 # define output file name
-outfile = here::here("open_source_measures", paste0(id, "_OSM"))
+outfile = here::here("open_source_measures", paste0(id, "_OSM.rds"))
 
 # read raw accelerometry data .mat file
 acc_df = read_acc_mat(fname)
@@ -66,6 +66,10 @@ out_ALL0 = SummarizedActigraphy::calculate_measures(
   calculate_mims = TRUE,
   verbose = TRUE)
 out_ALL0 = dplyr::rename(out_ALL0, HEADER_TIME_STAMP = time)
+# @MK on Jan 14, 2020: 
+# adding the belows line to address issue with MIMSunit package that allows 
+# negative values in the output, see issue: https://github.com/mHealthGroup/MIMSunit/issues/21
+out_ALL0$MIMS_UNIT[out_ALL0$MIMS_UNIT < 0] <- 0
 
 # ENMO 
 # > The ENMO at time t is defined as max [r(t) – 1, 0]. 
