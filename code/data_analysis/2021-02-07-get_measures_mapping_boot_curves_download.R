ca7ae1465964961fada3a09ff98af18dde2185b3
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-02-11 14:59:02 -0500

    organize files, add code to downloads data with additional bootstrap results

--- code/data_analysis/2021-02-07-get_measures_mapping_boot_curves_download.R
+++ code/data_analysis/2021-02-07-get_measures_mapping_boot_curves_download.R
@@ -8,6 +8,9 @@
 #' - quantile 1 - 0.025
 #' separately for each (k--size of spline basis, whether resample level 1) 
 #' pair of settings
+#' 
+#' Notes
+#' - cd /dcl01/smart/data/activity/blsa_mims/results
 
 rm(list = ls())
 
@@ -53,4 +56,59 @@ dat_out_df$AC <- rep(AC_seq, result_fnames_l)
 
 # save result to file 
 saveRDS(dat_out_df, paste0(here::here(), "/results/2021-02-07-boot_out_all.rds")) 
+
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+rm(list = ls())
+
+library(tidyverse)
+library(data.table)
+library(matrixStats)
+
+# read file names with bootstrap results
+result_fnames0 <- list.files(paste0(here::here(), "/results"), full.names = FALSE)
+result_fnames <- result_fnames0[which(grepl(pattern = "2021-02-09", result_fnames0))]
+result_fnames_l <- length(result_fnames)
+result_fnames_l
+
+# define object to store aggregated results from each file 
+dat_out_list <- vector(mode = "list", length = result_fnames_l)
+
+for (i in 1 : result_fnames_l){ # i <- 1
+  print(i)
+  fname_i <- result_fnames[i]
+  # pull bootstrap parameters from the current file name 
+  fname_i_ss     <- strsplit(fname_i, "_")[[1]]
+  measure_name_i <- fname_i_ss[3]
+  k_i            <- as.numeric(fname_i_ss[5])
+  step2repl_i    <- as.numeric(fname_i_ss[7])
+  rm0AC_i        <- as.numeric(fname_i_ss[9])
+  capAC_i        <- as.numeric(gsub("\\.rds", "", fname_i_ss[11]))
+  # pull bootstrap data results from the current file name
+  dat_i <- readRDS(paste0(here::here(), "/results/", fname_i))
+  # aggregate bootstrap data results from the current file name
+  dat_i_out <- data.frame(
+    val_mean = matrixStats::rowMeans2(dat_i),
+    val_median = matrixStats::rowMedians(dat_i),
+    val_q025 = matrixStats::rowQuantiles(dat_i, probs = 0.025),
+    val_q975 = matrixStats::rowQuantiles(dat_i, probs = 0.975)
+  )
+  dat_i_out$AC           = seq(0, by = 1, length.out = nrow(dat_i))
+  dat_i_out$k            = k_i
+  dat_i_out$step2repl    = step2repl_i
+  dat_i_out$measure_name = measure_name_i
+  dat_i_out$rm0AC        = rm0AC_i
+  dat_i_out$capAC        = capAC_i
+  dat_out_list[[i]] <- as.data.table(dat_i_out)
+}
+
+# combine list of data tables into one data table 
+dat_out_df <- rbindlist(dat_out_list) %>% as.data.frame()
+
+# save result to file 
+saveRDS(dat_out_df, paste0(here::here(), "/results/2021-02-11-boot_out_all.rds")) 
 
