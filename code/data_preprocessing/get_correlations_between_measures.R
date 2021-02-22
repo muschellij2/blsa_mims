aae6ac5fd934af11a3ce20428c4bea4e57e8bae3
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-02-22 18:28:18 -0500

    update script that computes correlations bwteeen the minute-level measures -- use wear minutes only

--- code/data_preprocessing/get_correlations_between_measures.R
+++ code/data_preprocessing/get_correlations_between_measures.R
@@ -1,13 +1,15 @@
-#' This script  computes 
-#' correlation between minute-level activity counts and 
-#' open-source minute-level measurements. It uses winsorized version of masterfile
-#' data with the measures.
+#' This script  computes correlation between minute-level activity counts and 
+#' open-source minute-level measurements. 
+#' 
+#' Note: 
+#' - We use winsorized version of masterfile data with the measures.
+#' - We use valid days only, and valid minutes within valid days. 
 #' 
 #' input files: 
 #' > /data_processed/2021-01-19-measures_masterfile_winsorized.rds
 #' 
 #' output files: 
-#' > /results/2021-01-19-correlations_between_measures.rds
+#' > /results/2021-02-22-correlations_between_measures.rds
 
 
 rm(list = ls())
@@ -20,6 +22,7 @@ dat <- readRDS(fpath_tmp)
 dim(dat)
 # Jan 18, 2021: 5791560      10
 # Jan 19, 2021: 6147240      10
+# Feb 22, 2021: 6147240      10
 
 # function to correlation matrix' upper triangle vector from data frame
 dat_to_corr_uppertri <- function(dat, corr_col_names = c("AC", "MIMS", "ENMO", "MAD", "AI")){
@@ -36,6 +39,7 @@ dat_to_corr_uppertri <- function(dat, corr_col_names = c("AC", "MIMS", "ENMO", "
 # compute correlations for all day data (24h)
 dat_corr_allday <- 
   dat %>% 
+  dplyr::filter(wear_flag == 1) %>% # added Feb 22, 2021
   dplyr::group_by(file_id) %>%
   dplyr::mutate(obs_cnt = n(), .before = dplyr::ends_with("_id")) %>%
   dplyr::ungroup() %>% 
@@ -48,6 +52,7 @@ dat_corr_allday <-
 # compute correlations while EXCLUDING 11pm - 5am 
 dat_corr_from11to5exclude <- 
   dat %>% 
+  dplyr::filter(wear_flag == 1) %>% # added Feb 22, 2021
   dplyr::filter(!(lubridate::hour(HEADER_TIME_STAMP) %in% c(23,0,1,2,3,4))) %>%
   dplyr::group_by(file_id) %>%
   dplyr::mutate(obs_cnt = n(), .before = dplyr::ends_with("_id")) %>%
@@ -69,6 +74,9 @@ length(unique(dat_corr_comb$file_id))
 # [1] 721
 
 # save as data frame
-fout_path <- paste0(here::here(), "/results/2021-01-19-correlations_between_measures.rds")
+# fout_path <- paste0(here::here(), "/results/2021-01-19-correlations_between_measures.rds")
+fout_path <- paste0(here::here(), "/results/2021-02-22-correlations_between_measures.rds")
 saveRDS(dat_corr_comb, fout_path)
+
+# get /dcl01/smart/data/activity/blsa_mims/results/2021-02-22-correlations_between_measures.rds /Users/martakaras/Dropbox/_PROJECTS/blsa_mims/results/2021-02-22-correlations_between_measures.rds 
 
