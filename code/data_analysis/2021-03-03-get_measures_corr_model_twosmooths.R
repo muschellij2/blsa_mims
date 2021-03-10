1ea6d08228bed9a0fc81a6be726ab145e4d152a0
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-03-10 12:39:12 -0500

    update corr model (two smooths)

--- code/data_analysis/2021-03-03-get_measures_corr_model_twosmooths.R
+++ code/data_analysis/2021-03-03-get_measures_corr_model_twosmooths.R
@@ -11,6 +11,18 @@ library(mgcv)
 options(scipen = 999)
 
 
+# function to transform correlation values rho into Fisher z-transformed values
+rho_to_z <- function (rho){
+  zval <- 0.5 * log((1 + rho)/(1 - rho))
+  return(zval)
+}
+# function to transform Fisher z-transformed values into correlation values rho
+z_to_rho <- function (z){
+  rhoval <- (exp(2 * z) - 1)/(1 + exp(2 * z))
+  return(rhoval)
+}
+
+
 # ------------------------------------------------------------------------------
 # read data 
 
@@ -63,18 +75,6 @@ name_labels  <- sapply(name_levels, function(val) paste0(str_split(val, "_")[[1]
 name_labels  <- paste0("(", name_labels, ")")
 name_labels2 <- paste0("corr", name_labels)
 name_labels3 <- paste0("y=corr", name_labels)
-
-# function to transform correlation values rho into Fisher z-transformed values
-rho_to_z <- function (rho){
-  zval <- 0.5 * log((1 + rho)/(1 - rho))
-  return(zval)
-}
-# function to transform Fisher z-transformed values into correlation values rho
-z_to_rho <- function (z){
-  rhoval <- (exp(2 * z) - 1)/(1 + exp(2 * z))
-  return(rhoval)
-}
-
 
 
 # ------------------------------------------------------------------------------
@@ -162,73 +162,89 @@ df <- rbind(
   compare_fit_gcv.ubre(4)
 )
 df
+#' - For 3/4 measures, the lowest gcv is for model 2: 
+#'   fit_tmp <- gam(y ~ s(age) + s(bmi) + sex, data = dat)
+#'   
+#' - For 1/4 measures, the lowest gcv is for model 6: 
+#'   fit_tmp <- gam(y ~ s(age) + bmi + sex, data = dat)
 
 
+
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-# MODEL FINAL FIT -- ESTIMATE for (A) AGE
+# MODEL FINAL FIT -- sumamrize estimates for (A) age
 
 use_rho_to_z = TRUE
 
-model_fitted_df_all <- data.frame()
-model_coef_df_all   <- data.frame()
+model_fitted_smooth_df_all <- data.frame()
+model_coef_fixed_df_all    <- data.frame()
 
 for (i in 1:4){ # i <- 3
-  # define the outcome Y 
+  
+  # define the outcome Y (current open-source measure name)
   y_lab_tmp <- var_names[i]
   print(y_lab_tmp)
   dat$y <- dat[, y_lab_tmp]
   if (use_rho_to_z) dat$y <- rho_to_z(dat$y)
-  # fit final model 
+  
+  # fit model 
   fit_i <- gam(y ~ s(age) + s(bmi) + sex, data = dat, method = "REML")
   fit_i_ss <- summary(fit_i)
-  # get estimated model coefficients  
-  fit_coef_i <- data.frame(
+  
+  # get model coefficients of fixed effects
+  fit_coef_fixed_i <- data.frame(
     coef_name = names(fit_i_ss$p.coeff),
     coef_est  = fit_i_ss$p.coeff,
     coef_pval = round(fit_i_ss$p.pv, 5)
   ) %>% mutate(
+    # convert coefficient back from z-Fisher transformed outcome scale to original outcome scale
     coef_est_rho = z_to_rho(coef_est), .after = coef_est,
     coef_est_rho = round(coef_est_rho, 5)
   )
-  fit_coef_i$name <- y_lab_tmp
-  model_coef_df_all <- rbind(model_coef_df_all, fit_coef_i)
-  # get estimated smooth effect (curve of fitted y + SE)
+  # add name for the outcome Y (current open-source measure name)
+  fit_coef_fixed_i$name   <- y_lab_tmp
+  model_coef_fixed_df_all <- rbind(model_coef_fixed_df_all, fit_coef_fixed_i)
+  
+  # get model coefficient of smooth effect 
+  # - seWithMean = TRUE yields the component smooths are shown with confidence 
+  #   intervals that include the uncertainty about the overall mean.
   plot_obj_i <- (plot(fit_i, shift = 0, seWithMean = TRUE, select = 1))[[1]]
-  fit_fitted_i <- data.frame(
+  fit_coef_smooth_i <- data.frame(
     age = plot_obj_i$x, 
-    y_fitted = plot_obj_i$fit, 
-    y_lwr = plot_obj_i$fit - plot_obj_i$se,
-    y_upr = plot_obj_i$fit + plot_obj_i$se
+    coef_fitted = plot_obj_i$fit, 
+    coef_lwr = plot_obj_i$fit - plot_obj_i$se,
+    coef_upr = plot_obj_i$fit + plot_obj_i$se
   ) %>% mutate(
-    # convert back to rho
-    y_fitted_rho = z_to_rho(y_fitted),
-    y_lwr_rho = z_to_rho(y_lwr),
-    y_upr_rho = z_to_rho(y_upr)
+    # convert back to rho 
+    coef_fitted_rho = z_to_rho(coef_fitted),
+    coef_lwr_rho = z_to_rho(coef_lwr),
+    coef_upr_rho = z_to_rho(coef_upr)
   ) 
-  # integrate over BMI effect to add to the values 
-  shift_intrc <- coef(fit_i)[1]
-  shift_bmi <- 0
-  shift_age <- 0
-  shift_sex   <- mean((dat$sex == "Male") * coef(fit_i)[3])
-  shift <- shift_intrc + shift_age + shift_sex + shift_bmi
-  fit_fitted_i <- fit_fitted_i %>%
+  
+  # shift model coefficient of smooth effect by estimates of fixed effects 
+  # (intercept, sex) to get values on the fitted outcome scale 
+  shift_intercept <- coef(fit_i)["(Intercept)"]
+  shift_sex       <- mean(as.numeric(dat$sex == "Male") * coef(fit_i)["sexMale"])
+  shift           <- shift_intercept + shift_sex
+  fit_coef_smooth_i <- 
+    fit_coef_smooth_i %>%
     mutate(
-      y_fitted_s = y_fitted + shift,
-      y_lwr_s = y_lwr + shift,
-      y_upr_s = y_upr + shift,
-      # convert back to rho
+      y_fitted_s = coef_fitted + shift,
+      y_lwr_s    = coef_lwr + shift,
+      y_upr_s    = coef_upr + shift, 
+      # convert back from z-Fisher transformed outcome scale to original outcome scale
       y_fitted_s_rho = z_to_rho(y_fitted_s),
-      y_lwr_s_rho = z_to_rho(y_lwr_s),
-      y_upr_s_rho = z_to_rho(y_upr_s),   
-      name = y_lab_tmp)
-  model_fitted_df_all <- rbind(model_fitted_df_all, fit_fitted_i)
+      y_lwr_s_rho    = z_to_rho(y_lwr_s),
+      y_upr_s_rho    = z_to_rho(y_upr_s),   
+      name           = y_lab_tmp
+    )
+  model_fitted_smooth_df_all <- rbind(model_fitted_smooth_df_all, fit_coef_smooth_i)
 }
 
 # mutate df factor
-model_fitted_df_all <- 
-  model_fitted_df_all %>%
+model_fitted_smooth_df_all <- 
+  model_fitted_smooth_df_all %>%
   mutate(name_fct = factor(name, levels = name_levels, labels = name_labels3)) 
 
 
@@ -244,34 +260,33 @@ theme_ggpr <- function(){
 }
 theme_set(theme_ggpr())
 
-
+# data frame with data points for the plot
 plt_df_points <- dat %>%
   select(all_of(c(var_names[1:4], "age", "bmi", "sex"))) %>%
   pivot_longer(cols = -c(age, bmi, sex)) %>%
   mutate(name_fct = factor(name, levels = name_levels, labels = name_labels3)) 
 
-ggplot(model_fitted_df_all, aes(x = age, y = y_fitted_rho)) + 
-  geom_ribbon(aes(ymin = y_lwr_rho, ymax = y_upr_rho), alpha = 0.2) + 
+# Plot: Age effect on y=z(corr(M1,M2))
+ggplot(model_fitted_smooth_df_all, aes(x = age, y = coef_fitted)) + 
+  geom_ribbon(aes(ymin = coef_lwr, ymax = coef_upr), alpha = 0.2) + 
   geom_line() + 
   facet_wrap(~ name_fct, ncol = 2) + 
   theme(legend.position = "none") + 
-  scale_color_futurama() + 
-  # scale_y_continuous(limits = c(0.5, 1)) + 
-  # labs(y = "Age effect on y\n ", x = "Age") + 
-  labs(y = "Age effect on y", x = "Age")
+  labs(y = "Age effect on y (=z(corr))", x = "Age")
 
-ggplot(model_fitted_df_all, aes(x = age, y = y_fitted_s_rho)) + 
-  geom_ribbon(aes(ymin = y_lwr_s_rho, ymax = y_upr_s_rho), alpha = 0.2) + 
+# Plot: Age effect on y=corr(M1,M2)
+ggplot(model_fitted_smooth_df_all, aes(x = age, y = coef_fitted_rho)) + 
+  geom_ribbon(aes(ymin = coef_lwr_rho, ymax = coef_upr_rho), alpha = 0.2) + 
   geom_line() + 
   facet_wrap(~ name_fct, ncol = 2) + 
   theme(legend.position = "none") + 
   scale_color_futurama() + 
-  scale_y_continuous(limits = c(0.5, 1)) + 
   # labs(y = "Age effect on y\n(shifted)", x = "Age")  + 
   labs(y = "Age effect on y", x = "Age")
 
+# Plot: Age effect on y=corr(M1,M2), shifted
 plt3 <- 
-  ggplot(model_fitted_df_all, aes(x = age, y = y_fitted_s_rho)) + 
+  ggplot(model_fitted_smooth_df_all, aes(x = age, y = y_fitted_s_rho)) + 
   geom_point(data = plt_df_points, aes(x = age, y = value, color = name_fct), alpha = 0.1) +
   geom_ribbon(aes(ymin = y_lwr_s_rho, ymax = y_upr_s_rho), alpha = 0.2) + 
   geom_line() + 
@@ -279,8 +294,7 @@ plt3 <-
   theme(legend.position = "none") + 
   scale_color_futurama() + 
   scale_y_continuous(limits = c(0.5, 1)) + 
-  # labs(y = "Age effect on y\n(shifted)", x = "Age") + 
-  labs(y = "Age effect on y", x = "Age")
+  labs(y = "Age effect on y (shifted)", x = "Age")
 plt3
 
 
@@ -288,115 +302,119 @@ plt3
 plt_path <- paste0(here::here(), "/results_figures/measures_corr_model_age_effect_twosmooths.png")
 ggsave(filename = plt_path, plot = plt3, width = 8, height = 6.4)
 
-rm(model_fitted_df_all)
-rm(plt_df_points)
+rm(model_fitted_smooth_df_all)
+
 
 # MODEL FINAL FIT -- SUMMARIZE COEFS -------------------------------------------
 
-rownames(model_coef_df_all) <- NULL
-model_coef_df_all
-
+rownames(model_coef_fixed_df_all) <- NULL
+model_coef_fixed_df_all
+rm(model_coef_fixed_df_all)
 
 
 
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
-# MODEL FINAL FIT -- ESTIMATE for (B) BMI
+# MODEL FINAL FIT -- sumamrize estimates for (B) bmi
 
 use_rho_to_z = TRUE
 
-model_fitted_df_all <- data.frame()
-model_coef_df_all   <- data.frame()
+model_fitted_smooth_df_all <- data.frame()
+model_coef_fixed_df_all    <- data.frame()
 
 for (i in 1:4){ # i <- 3
-  # define the outcome Y 
+  
+  # define the outcome Y (current open-source measure name)
   y_lab_tmp <- var_names[i]
   print(y_lab_tmp)
   dat$y <- dat[, y_lab_tmp]
   if (use_rho_to_z) dat$y <- rho_to_z(dat$y)
-  # fit final model 
+  
+  # fit model 
   fit_i <- gam(y ~ s(age) + s(bmi) + sex, data = dat, method = "REML")
   fit_i_ss <- summary(fit_i)
-  # get estimated model coefficients  
-  fit_coef_i <- data.frame(
+  
+  # get model coefficients of fixed effects
+  fit_coef_fixed_i <- data.frame(
     coef_name = names(fit_i_ss$p.coeff),
     coef_est  = fit_i_ss$p.coeff,
     coef_pval = round(fit_i_ss$p.pv, 5)
   ) %>% mutate(
+    # convert coefficient back from z-Fisher transformed outcome scale to original outcome scale
     coef_est_rho = z_to_rho(coef_est), .after = coef_est,
     coef_est_rho = round(coef_est_rho, 5)
   )
-  fit_coef_i$name <- y_lab_tmp
-  model_coef_df_all <- rbind(model_coef_df_all, fit_coef_i)
-  # get estimated smooth effect (curve of fitted y + SE)
-  plot_obj_i <- (plot(fit_i, shift = 0, seWithMean = TRUE, select = 2))[[2]]
-  fit_fitted_i <- data.frame(
+  # add name for the outcome Y (current open-source measure name)
+  fit_coef_fixed_i$name   <- y_lab_tmp
+  model_coef_fixed_df_all <- rbind(model_coef_fixed_df_all, fit_coef_fixed_i)
+  
+  # get model coefficient of smooth effect 
+  # - seWithMean = TRUE yields the component smooths are shown with confidence 
+  #   intervals that include the uncertainty about the overall mean.
+  plot_obj_i <- (plot(fit_i, shift = 0, seWithMean = TRUE, select = 1))[[2]]
+  fit_coef_smooth_i <- data.frame(
     bmi = plot_obj_i$x, 
-    y_fitted = plot_obj_i$fit, 
-    y_lwr = plot_obj_i$fit - plot_obj_i$se,
-    y_upr = plot_obj_i$fit + plot_obj_i$se
+    coef_fitted = plot_obj_i$fit, 
+    coef_lwr = plot_obj_i$fit - plot_obj_i$se,
+    coef_upr = plot_obj_i$fit + plot_obj_i$se
   ) %>% mutate(
-    # convert back to rho
-    y_fitted_rho = z_to_rho(y_fitted),
-    y_lwr_rho = z_to_rho(y_lwr),
-    y_upr_rho = z_to_rho(y_upr)
+    # convert back to rho 
+    coef_fitted_rho = z_to_rho(coef_fitted),
+    coef_lwr_rho = z_to_rho(coef_lwr),
+    coef_upr_rho = z_to_rho(coef_upr)
   ) 
-  # integrate over BMI effect to add to the values 
-  shift_intrc <- coef(fit_i)[1]
-  shift_bmi <- 0
-  shift_age <- 0
-  shift_sex   <- mean((dat$sex == "Male") * coef(fit_i)[3])
-  shift <- shift_intrc + shift_age + shift_sex + shift_bmi
-  fit_fitted_i <- fit_fitted_i %>%
+  
+  # shift model coefficient of smooth effect by estimates of fixed effects 
+  # (intercept, sex) to get values on the fitted outcome scale 
+  shift_intercept <- coef(fit_i)["(Intercept)"]
+  shift_sex       <- mean(as.numeric(dat$sex == "Male") * coef(fit_i)["sexMale"])
+  shift           <- shift_intercept + shift_sex
+  fit_coef_smooth_i <- 
+    fit_coef_smooth_i %>%
     mutate(
-      y_fitted_s = y_fitted + shift,
-      y_lwr_s = y_lwr + shift,
-      y_upr_s = y_upr + shift,
-      # convert back to rho
+      y_fitted_s = coef_fitted + shift,
+      y_lwr_s    = coef_lwr + shift,
+      y_upr_s    = coef_upr + shift, 
+      # convert back from z-Fisher transformed outcome scale to original outcome scale
       y_fitted_s_rho = z_to_rho(y_fitted_s),
-      y_lwr_s_rho = z_to_rho(y_lwr_s),
-      y_upr_s_rho = z_to_rho(y_upr_s),   
-      name = y_lab_tmp)
-  model_fitted_df_all <- rbind(model_fitted_df_all, fit_fitted_i)
+      y_lwr_s_rho    = z_to_rho(y_lwr_s),
+      y_upr_s_rho    = z_to_rho(y_upr_s),   
+      name           = y_lab_tmp
+    )
+  model_fitted_smooth_df_all <- rbind(model_fitted_smooth_df_all, fit_coef_smooth_i)
 }
 
 # mutate df factor
-model_fitted_df_all <- 
-  model_fitted_df_all %>%
+model_fitted_smooth_df_all <- 
+  model_fitted_smooth_df_all %>%
   mutate(name_fct = factor(name, levels = name_levels, labels = name_labels3)) 
 
 
 
 # MODEL FINAL FIT -- PLOT ------------------------------------------------------
 
-plt_df_points <- dat %>%
-  select(all_of(c(var_names[1:4], "age", "bmi", "sex"))) %>%
-  pivot_longer(cols = -c(age, bmi, sex)) %>%
-  mutate(name_fct = factor(name, levels = name_levels, labels = name_labels3)) 
+# Plot: BMI effect on y=z(corr(M1,M2))
+ggplot(model_fitted_smooth_df_all, aes(x = bmi, y = coef_fitted)) + 
+  geom_ribbon(aes(ymin = coef_lwr, ymax = coef_upr), alpha = 0.2) + 
+  geom_line() + 
+  facet_wrap(~ name_fct, ncol = 2) + 
+  theme(legend.position = "none") + 
+  labs(y = "BMI effect on y (=z(corr))", x = "BMI")
 
-ggplot(model_fitted_df_all, aes(x = bmi, y = y_fitted_rho)) + 
-  geom_ribbon(aes(ymin = y_lwr_rho, ymax = y_upr_rho), alpha = 0.2) + 
+# Plot: BMI effect on y=corr(M1,M2)
+ggplot(model_fitted_smooth_df_all, aes(x = bmi, y = coef_fitted_rho)) + 
+  geom_ribbon(aes(ymin = coef_lwr_rho, ymax = coef_upr_rho), alpha = 0.2) + 
   geom_line() + 
   facet_wrap(~ name_fct, ncol = 2) + 
   theme(legend.position = "none") + 
   scale_color_futurama() + 
-  # scale_y_continuous(limits = c(0.5, 1)) + 
-  # labs(y = "BMI effect on y\n ", x = "BMI") + 
-  labs(y = "BMI effect on y", x = "BMI")
-
-ggplot(model_fitted_df_all, aes(x = bmi, y = y_fitted_s_rho)) + 
-  geom_ribbon(aes(ymin = y_lwr_s_rho, ymax = y_upr_s_rho), alpha = 0.2) + 
-  geom_line() + 
-  facet_wrap(~ name_fct, ncol = 2) + 
-  theme(legend.position = "none") + 
-  scale_color_futurama() + 
-  scale_y_continuous(limits = c(0.5, 1)) + 
   # labs(y = "BMI effect on y\n(shifted)", x = "BMI")  + 
   labs(y = "BMI effect on y", x = "BMI")
 
+# Plot: BMI effect on y=corr(M1,M2), shifted
 plt3 <- 
-  ggplot(model_fitted_df_all, aes(x = bmi, y = y_fitted_s_rho)) + 
+  ggplot(model_fitted_smooth_df_all, aes(x = bmi, y = y_fitted_s_rho)) + 
   geom_point(data = plt_df_points, aes(x = bmi, y = value, color = name_fct), alpha = 0.1) +
   geom_ribbon(aes(ymin = y_lwr_s_rho, ymax = y_upr_s_rho), alpha = 0.2) + 
   geom_line() + 
@@ -404,8 +422,7 @@ plt3 <-
   theme(legend.position = "none") + 
   scale_color_futurama() + 
   scale_y_continuous(limits = c(0.5, 1)) + 
-  # labs(y = "BMI effect on y\n(shifted)", x = "BMI") + 
-  labs(y = "BMI effect on y", x = "BMI")
+  labs(y = "BMI effect on y (shifted)", x = "BMI")
 plt3
 
 
@@ -413,14 +430,11 @@ plt3
 plt_path <- paste0(here::here(), "/results_figures/measures_corr_model_bmi_effect_twosmooths.png")
 ggsave(filename = plt_path, plot = plt3, width = 8, height = 6.4)
 
+rm(model_fitted_smooth_df_all)
+rm(plt_df_points)
 
 
+# MODEL FINAL FIT -- SUMMARIZE COEFS -------------------------------------------
 
-# ------------------------------------------------------------------------------
-# MODEL FINAL FIT -- SUMMARIZE COEFS
-
-rownames(model_coef_df_all) <- NULL
-model_coef_df_all
-
-
-
+rownames(model_coef_fixed_df_all) <- NULL
+model_coef_fixed_df_all
