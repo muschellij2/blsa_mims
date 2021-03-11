e75831c0ca3f82c9280fdded147046215dc77883
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-03-10 20:20:40 -0500

    add code for sensitivity analysis

--- code/data_analysis/2021-03-05-get_measures_mapping_boot_curves_download.R
+++ code/data_analysis/2021-03-05-get_measures_mapping_boot_curves_download.R
@@ -17,66 +17,17 @@ library(matrixStats)
 
 # read file names with bootstrap results
 result_fnames0  <- list.files(paste0(here::here(), "/results"), full.names = FALSE)
-result_fnames   <- result_fnames0[which(grepl(pattern = "2021-03-03", result_fnames0))]
+result_fnames   <- result_fnames0[which(grepl(pattern = "2021-03-05", result_fnames0))]
 result_fnames_l <- length(result_fnames)
 result_fnames_l
+
+# define significance level used to create confidence intervals 
+sign_alpha <- 0.05
 
 # define object to store aggregated results from each file 
 dat_out_list <- vector(mode = "list", length = result_fnames_l)
 
-for (i in 1 : result_fnames_l){
-  print(i)
-  fname_i <- result_fnames[i]
-  # pull bootstrap parameters from the current file name 
-  fname_i_ss <- strsplit(fname_i, "_")[[1]]
-  measure_name_i <- fname_i_ss[3]
-  k_i <- as.numeric(fname_i_ss[5])
-  step2repl_i <- as.numeric(gsub("\\.rds", "", fname_i_ss[7]))
-  # pull bootstrap data results from the current file name
-  dat_i <- readRDS(paste0(here::here(), "/results/", fname_i))
-  # aggregate bootstrap data results from the current file name
-  dat_i_out <- data.frame(
-    val_mean = matrixStats::rowMeans2(dat_i),
-    val_median = matrixStats::rowMedians(dat_i),
-    val_q025 = matrixStats::rowQuantiles(dat_i, probs = 0.025),
-    val_q975 = matrixStats::rowQuantiles(dat_i, probs = 0.975)
-  )
-  dat_i_out$k = k_i
-  dat_i_out$step2repl = step2repl_i
-  dat_i_out$measure_name = measure_name_i
-  dat_out_list[[i]] <- as.data.table(dat_i_out)
-}
-
-# combine list of data tables into one data table 
-dat_out_df <- rbindlist(dat_out_list) %>% as.data.frame()
-AC_seq <- seq(from = 0, by = 1, length.out = 36249)
-dat_out_df$AC <- rep(AC_seq, result_fnames_l)
-
-# save result to file 
-saveRDS(dat_out_df, paste0(here::here(), "/results/2021-02-07-boot_out_all.rds")) 
-
-
-
-# ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------
-
-rm(list = ls())
-
-library(tidyverse)
-library(data.table)
-library(matrixStats)
-
-# read file names with bootstrap results
-result_fnames0 <- list.files(paste0(here::here(), "/results"), full.names = FALSE)
-result_fnames <- result_fnames0[which(grepl(pattern = "2021-02-09", result_fnames0))]
-result_fnames_l <- length(result_fnames)
-result_fnames_l
-
-# define object to store aggregated results from each file 
-dat_out_list <- vector(mode = "list", length = result_fnames_l)
-
-for (i in 1 : result_fnames_l){ # i <- 1
+for (i in 1 : result_fnames_l){ # i <- 5
   print(i)
   fname_i <- result_fnames[i]
   # pull bootstrap parameters from the current file name 
@@ -90,25 +41,32 @@ for (i in 1 : result_fnames_l){ # i <- 1
   dat_i <- readRDS(paste0(here::here(), "/results/", fname_i))
   # aggregate bootstrap data results from the current file name
   dat_i_out <- data.frame(
-    val_mean = matrixStats::rowMeans2(dat_i, na.rm = TRUE),
+    val_mean   = matrixStats::rowMeans2(dat_i, na.rm = TRUE),
     val_median = matrixStats::rowMedians(dat_i, na.rm = TRUE),
-    val_q025 = matrixStats::rowQuantiles(dat_i, probs = 0.025, na.rm = TRUE),
-    val_q975 = matrixStats::rowQuantiles(dat_i, probs = 0.975, na.rm = TRUE)
+    val_q025   = matrixStats::rowQuantiles(dat_i, probs = sign_alpha, na.rm = TRUE),
+    val_q975   = matrixStats::rowQuantiles(dat_i, probs = (1 - sign_alpha/2), na.rm = TRUE),
+    val_cnt    = apply(dat_i, 1, function(row_j) sum(!is.na(row_j)))
   )
-  dat_i_out$AC           = seq(0, by = 1, length.out = nrow(dat_i))
-  dat_i_out$k            = k_i
-  dat_i_out$step2repl    = step2repl_i
   dat_i_out$measure_name = measure_name_i
-  dat_i_out$rm0AC        = rm0AC_i
-  dat_i_out$capAC        = capAC_i
+  dat_i_out$k = k_i
+  dat_i_out$step2repl = step2repl_i
+  dat_i_out$rm0AC = rm0AC_i
+  dat_i_out$capAC = capAC_i
   dat_out_list[[i]] <- as.data.table(dat_i_out)
 }
 
 # combine list of data tables into one data table 
 dat_out_df <- rbindlist(dat_out_list) %>% as.data.frame()
-dim(dat_out_df)
+nrow(dat_out_df)
+# Mar 10: [1] 504160
+
+AC_seq <- seq(from = 0, to = 15754, by = 1)
+nrow(dat_out_df) / length(AC_seq)
+
+dat_out_df$AC <- rep(AC_seq, result_fnames_l)
 
 # save result to file 
-saveRDS(dat_out_df, paste0(here::here(), "/results/2021-02-11-boot_out_all.rds")) 
+saveRDS(dat_out_df, paste0(here::here(), "/results/2021-03-10-boot_out_all.rds")) 
 
-# get /dcl01/smart/data/activity/blsa_mims/results/2021-02-11-boot_out_all.rds /Users/martakaras/Dropbox/_PROJECTS/blsa_mims/results/2021-02-11-boot_out_all.rds
+
+# get /dcl01/smart/data/activity/blsa_mims/results/2021-03-10-boot_out_all.rds /Users/martakaras/Dropbox/_PROJECTS/blsa_mims/results/2021-03-10-boot_out_all.rds
---
+++ code/data_analysis/2021-03-10-get_measures_mapping_boot_sensitivity.R
@@ -0,0 +1,167 @@
+#!/usr/bin/env Rscript
+
+#' Script to run sensitivity analysis of open-source measures in estimating
+#' AC thresholds.
+#' 
+#' Notes: 
+#' 
+#' cd $mims
+#' cd code/data_analysis
+#' Rnosave 2021-03-10-get_measures_mapping_boot_sensitivity.R -t 1-3 -N JOB_sensit
+
+# rm(list = ls())
+library(tidyverse)
+library(knitr)
+library(gridExtra)
+library(ROCR)
+library(cowplot)
+
+dat_acc_fpath <- paste0(here::here(), "/data_processed/2021-03-03-measures_masterfile_winsorized.rds")
+dat_acc <- readRDS(dat_acc_fpath) 
+dat_acc <- dat_acc %>% dplyr::filter(wear_and_valid_flag == 1)
+
+# current job params (specific to array job index)
+job_idx = as.numeric(Sys.getenv("SGE_TASK_ID")) # job_idx <- 2
+AC_thresh <- c(1, 1853, 2303)[job_idx]
+message(paste0("AC_thresh = ", AC_thresh))
+
+measure_grid_l <- 10
+get_auc <- TRUE
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Define functions and objects needed to run sensitivity analysis
+
+# function to evaluate threshold 
+eval_measure_threshold <- function(AC_vec, 
+                                   measure_vec, 
+                                   AC_thresh,
+                                   measure_thresh_grid, 
+                                   measure_label,
+                                   get_auc){
+  
+  message(measure_label)
+  
+  # define true AC outcomes
+  true1_vec       <- as.numeric(AC_vec < AC_thresh)
+  true1_which_vec <- which(AC_vec < AC_thresh)
+  true0_which_vec <- which(!(AC_vec < AC_thresh))
+  
+  # define objects to store results 
+  out_df         <- data.frame(measure_thresh_grid = measure_thresh_grid)
+  out_df$out_sens = out_df$out_spec = out_df$out_accr = out_df$out_auc = NA
+  
+  # iterate over measure grid values
+  measure_thresh_grid_l <- length(measure_thresh_grid)
+  for (i in 1 : measure_thresh_grid_l){ # i <- 2
+    message(i)
+    est1_vec           <- as.numeric(measure_vec < measure_thresh_grid[i])
+    out_df$out_sens[i] <- mean(est1_vec[true1_which_vec])
+    out_df$out_spec[i] <- 1 - mean(est1_vec[true0_which_vec])
+    out_df$out_accr[i] <- mean(true1_vec == est1_vec)
+    if (get_auc){
+      # prediction(predictions, labels, label.ordering = NULL)
+      pred <- ROCR::prediction(est1_vec, true1_vec)
+      auc_obj <- ROCR::performance(pred, "auc")
+      out_df$out_auc[i] <- as.numeric(auc_obj@y.values)
+    }
+  }
+  
+  out_df$measure_name <- measure_label
+  return(out_df)
+}
+
+
+# define parameters grid 
+param_df <- data.frame()
+
+# AC_thresh = 1
+param_df <- rbind(param_df, data.frame(
+  AC_thresh        = rep(1, 4),
+  measure_name     = c("MIMS", "ENMO", "MAD", "AI"),
+  measure_grid_min = rep(0, 4),
+  measure_grid_max = c(1, 0.05, 0.025, 1)
+))
+
+# AC_thresh = 1853
+param_df <- rbind(param_df, data.frame(
+  AC_thresh        = rep(1853, 4),
+  measure_name     = c("MIMS", "ENMO", "MAD", "AI"),
+  measure_grid_min = c(9.5, 0.02, 0.03, 3),
+  measure_grid_max = c(11.5, 0.05, 0.05, 4)
+))
+
+# AC_thresh = 2303
+param_df <- rbind(param_df, data.frame(
+  AC_thresh        = rep(2303, 4),
+  measure_name     = c("MIMS", "ENMO", "MAD", "AI"),
+  measure_grid_min = c(11.5, 0.02, 0.035, 4),
+  measure_grid_max = c(13.5, 0.06, 0.055, 5)
+))
+
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# Run sensitivity analysis 
+
+# pull parameters grid 
+param_df_sub  <- param_df[param_df[, 1] == AC_thresh, ] 
+if (nrow(param_df_sub) == 0) stop("nrow(param_df_sub) == 0")
+grid_min <- param_df_sub %>% pull(measure_grid_min)
+grid_max <- param_df_sub %>% pull(measure_grid_max)
+names(grid_min) <- param_df_sub %>% pull(measure_name)
+names(grid_max) <- param_df_sub %>% pull(measure_name)
+
+# run performance stats
+perf_df_MIMS <- eval_measure_threshold(
+  AC_vec = dat_acc$AC, 
+  measure_vec = dat_acc$MIMS, 
+  AC_thresh = AC_thresh,
+  measure_thresh_grid = seq(grid_min["MIMS"], grid_max["MIMS"], length.out = measure_grid_l), 
+  measure_label = "MIMS",
+  get_auc = get_auc
+)
+
+perf_df_ENMO <- eval_measure_threshold(
+  AC_vec = dat_acc$AC, 
+  measure_vec = dat_acc$ENMO, 
+  AC_thresh = AC_thresh,
+  measure_thresh_grid = seq(grid_min["ENMO"], grid_max["ENMO"], length.out = measure_grid_l), 
+  measure_label = "ENMO",
+  get_auc = get_auc
+)
+  
+perf_df_MAD <- eval_measure_threshold(
+  AC_vec = dat_acc$AC, 
+  measure_vec = dat_acc$MAD, 
+  AC_thresh = AC_thresh,
+  measure_thresh_grid = seq(grid_min["MAD"], grid_max["MAD"], length.out = measure_grid_l), 
+  measure_label = "MAD",
+  get_auc = get_auc
+)
+
+perf_df_AI <- eval_measure_threshold(
+  AC_vec = dat_acc$AC, 
+  measure_vec = dat_acc$AI, 
+  AC_thresh = AC_thresh,
+  measure_thresh_grid = seq(grid_min["AI"], grid_max["AI"], length.out = measure_grid_l), 
+  measure_label = "AI",
+  get_auc = get_auc
+)
+
+# save results to file 
+perf_df_comb <- rbind(
+  perf_df_MIMS,
+  perf_df_ENMO,
+  perf_df_MAD,
+  perf_df_AI
+) %>% 
+  as.data.frame() %>%
+  mutate(AC_thresh = AC_thresh) 
+  
+out_fname <- paste0("2021-03-10-measures_mapping_sensitivity_ACthresh_", AC_thresh, ".rds")
+saveRDS(perf_df_comb, paste0(here::here(), "/results/", out_fname))
+
+message("FINISHED and saved.")
