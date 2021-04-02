c343cde24fa237d508f6ef8d6d7ff6703e49757e
Marta Karas <martakaras@Martas-MacBook-Pro.local>
2021-04-01 22:24:17 -0400

    add the mapping

--- code/data_preprocessing/prepare_measures_masterfile_winsorized_imp_mapped.R
+++ code/data_preprocessing/prepare_measures_masterfile_winsorized_imp_mapped.R
@@ -1,84 +1,57 @@
-#' This script produces "master file" with minute-level values 
-#' of 5 measures: 
+#' This script performs mappings: 
 #' 
-#' - AC, MIMS, MAD, ENMO, AI,
-#' 
-#' after filtering data to keep only: 
-#' 
-#' - data for participants for which both activity counts (AC) and open source
-#'   measures (OSM) data exist
-#' - data for participants who have >= 3 valid days
-#' - data form valid days only
-#' 
-#' and after data winsorization. 
-#' 
-#' input files: 
-#' - /data_processed/2021-03-25-measures_masterfile.rds
-#' 
-#' out file: 
-#' - /data_processed/2021-03-25-measures_masterfile_winsorized.rds
-#' 
-#' Notes: 
-#' - use: cd /dcl01/smart/data/activity/blsa_mims
+#' mapping g: {MIMS, ENMO, MAD, AI} -> AC
+#' mapping f: AC -> {MIMS, ENMO, MAD, AI}
 
 rm(list = ls())
-library(dplyr)
-library(data.table)
-library(lubridate)
-library(arctools)
-library(stringr)
-library(here)
+library(tidyverse)
 options(scipen=999)
 
+# source script with mapping
+source(paste0(here::here(), "/code/data_preprocessing/measures_mapping_FUNC.R"))
 
-# ------------------------------------------------------------------------------
-
-# read minute-level measures data (winsorized)
-dat_acc_fpath <- paste0(here::here(), "/data_processed/2021-03-25-measures_masterfile_winsorized.rds")
+# read minute-level measures data (winsorized, imputed)
+dat_acc_fpath <- paste0(here::here(), "/data_processed/2021-04-01-measures_masterfile_winsorized_imp.rds")
 dat_acc <- readRDS(dat_acc_fpath) 
-dat_acc <- dat_acc %>% dplyr::filter(wear_and_valid_flag == 1)
-x <- dat_acc$AC
+nrow(dat_acc) / 1440
+str(dat_acc)
 
+
+# mapping g: {MIMS, ENMO, MAD, AI} -> AC
 t1 <- Sys.time()
-dat_acc$AC_hat_MIMS <- MIMS_to_AC_map(dat_acc$MIMS)
-dat_acc$AC_hat_ENMO <- ENMO_to_AC_map(dat_acc$ENMO)
-dat_acc$AC_hat_MAD  <- MAD_to_AC_map(dat_acc$MAD)
-dat_acc$AC_hat_AI   <- AI_to_AC_map(dat_acc$AI)
+dat_acc$AC_hat_from_MIMS <- MIMS_to_AC_map(dat_acc$MIMS)
+dat_acc$AC_hat_from_ENMO <- ENMO_to_AC_map(dat_acc$ENMO)
+dat_acc$AC_hat_from_MAD  <- MAD_to_AC_map(dat_acc$MAD)
+dat_acc$AC_hat_from_AI   <- AI_to_AC_map(dat_acc$AI)
 t2 <- Sys.time()
 t2 - t1
 c(
-  cor(dat_acc$AC, dat_acc$AC_hat_MIMS),
-  cor(dat_acc$AC, dat_acc$AC_hat_ENMO),
-  cor(dat_acc$AC, dat_acc$AC_hat_MAD),
-  cor(dat_acc$AC, dat_acc$AC_hat_AI)
+  cor(dat_acc$AC, dat_acc$AC_hat_from_MIMS),
+  cor(dat_acc$AC, dat_acc$AC_hat_from_ENMO),
+  cor(dat_acc$AC, dat_acc$AC_hat_from_MAD),
+  cor(dat_acc$AC, dat_acc$AC_hat_from_AI)
 )
+# [1] 0.9913308 0.7477262 0.9161420 0.9691883
 
+
+# mapping f: AC -> {MIMS, ENMO, MAD, AI}
 t1 <- Sys.time()
-dat_acc$MIMS_hat <- AC_to_MIMS_map(dat_acc$AC)
-dat_acc$ENMO_hat <- AC_to_ENMO_map(dat_acc$AC)
-dat_acc$MAD_hat  <- AC_to_MAD_map(dat_acc$AC)
-dat_acc$AI_hat   <- AC_to_AI_map(dat_acc$AC)
+dat_acc$MIMS_hat_from_AC <- AC_to_MIMS_map(dat_acc$AC)
+dat_acc$ENMO_hat_from_AC <- AC_to_ENMO_map(dat_acc$AC)
+dat_acc$MAD_hat_from_AC  <- AC_to_MAD_map(dat_acc$AC)
+dat_acc$AI_hat_from_AC   <- AC_to_AI_map(dat_acc$AC)
 t2 <- Sys.time()
 t2 - t1
 c(
-  cor(dat_acc$MIMS, dat_acc$MIMS_hat),
-  cor(dat_acc$ENMO, dat_acc$ENMO_hat),
-  cor(dat_acc$MAD, dat_acc$MAD_hat),
-  cor(dat_acc$AI, dat_acc$AI_hat)
+  cor(dat_acc$MIMS, dat_acc$MIMS_hat_from_AC),
+  cor(dat_acc$ENMO, dat_acc$ENMO_hat_from_AC),
+  cor(dat_acc$MAD, dat_acc$MAD_hat_from_AC),
+  cor(dat_acc$AI, dat_acc$AI_hat_from_AC)
 )
+# [1] 0.9925897 0.7664135 0.9075113 0.9707521
 
+# save as data frame
+fpath_out <- paste0(here::here(), "/data_processed/2021-04-01-measures_masterfile_winsorized_imp_mapped.rds")
+saveRDS(dat_acc, fpath_out)
 
-t1 <- Sys.time()
-dat_acc$MIMS_hat <- AC_to_MIMS_map(dat_acc$AC, set0_for_AC0 = FALSE)
-dat_acc$ENMO_hat <- AC_to_ENMO_map(dat_acc$AC, set0_for_AC0 = FALSE)
-dat_acc$MAD_hat  <- AC_to_MAD_map(dat_acc$AC, set0_for_AC0 = FALSE)
-dat_acc$AI_hat   <- AC_to_AI_map(dat_acc$AC, set0_for_AC0 = FALSE)
-t2 <- Sys.time()
-t2 - t1
-c(
-  cor(dat_acc$MIMS, dat_acc$MIMS_hat),
-  cor(dat_acc$ENMO, dat_acc$ENMO_hat),
-  cor(dat_acc$MAD, dat_acc$MAD_hat),
-  cor(dat_acc$AI, dat_acc$AI_hat)
-)
-
+# put /Users/martakaras/Dropbox/_PROJECTS/blsa_mims/data_processed/2021-04-01-measures_masterfile_winsorized_imp_mapped.rds /dcl01/smart/data/activity/blsa_mims/data_processed/2021-04-01-measures_masterfile_winsorized_imp_mapped.rds 
